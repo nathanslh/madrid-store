68f675e527b5e52e134073379420e9c60aba6825
nathanslh <nathansilalahi2415@gmail.com>
2025-09-07 13:58:57 +0700

    Final Tugas Individu 2

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406496170',
+        'name': 'Natan Harum Panogu Silalahi',
+        'class': 'PBP E'
+    }
+    return render(request, "main.html", context)
