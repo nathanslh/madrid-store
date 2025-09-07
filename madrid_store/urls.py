ab3f36a07b07a963b580daad790fc46af3be5305
nathanslh <nathansilalahi2415@gmail.com>
2025-09-07 14:08:45 +0700

    Fix Eror

--- madrid_store/urls.py
+++ madrid_store/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from main.views import show_main
+
+app_name = 'main'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', show_main, name='show_main'),
 ]
