f53c0105e1c2a105b08c1a703771390e4edd5420
nathanslh <nathansilalahi2415@gmail.com>
2025-09-07 14:25:24 +0700

    Fixed Error

--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2406496170',
         'name': 'Natan Harum Panogu Silalahi',
+        'npm' : '2406496170',
         'class': 'PBP E'
     }
     return render(request, "main.html", context)
