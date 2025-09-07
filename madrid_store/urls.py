2e0167c36ce5bc380cb6d2f9fd7ddf969ca8f36e
nathanslh <nathansilalahi2415@gmail.com>
2025-09-07 14:13:06 +0700

    Progress

--- madrid_store/urls.py
+++ madrid_store/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from main.views import show_main
-
-app_name = 'main'
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
 ]
